c27ebff2a4d9b62ab00a2d1d1635044ffdec71c5
摄氏0℃ <chenxu7601257@qq.com>
2017-04-09 21:11:35 +0800

    Angular2 Services & OnInit event
    Angular2 Services & OnInit event

--- dev/contacts/contact-list.component.ts
+++ dev/contacts/contact-list.component.ts
@@ -1,5 +1,7 @@
-import {Component} from 'angular2/core';
-import {ContactComponent} from './contact.component'
+import {Component, OnInit} from 'angular2/core';
+import {ContactComponent} from './contact.component';
+import {ContactService} from './contact.service';
+import {Contact} from './contact';
 
 @Component({
     selector:'contact-list',
@@ -14,25 +16,31 @@ import {ContactComponent} from './contact.component'
         </ul>
         <contact [contact]="selectedContact"></contact>
     `,
-    directives:[ContactComponent]
+    directives:[ContactComponent],
+    providers:[ContactService],
+    styleUrls:['../src/css/app.css']
 })
 
-export class ContactListComponent{
-    public contacts=[{
-        firstName:'Max', lastName:'Schwarzuller',phone:'010-45443443',email:'max@schwar.com'
-    },{
-        firstName:'Jacob', lastName:'Chenxu',phone:'028-45443443',email:'jacob@schwar.com'
-    },{
-        firstName:'Dasiy', lastName:'Maersk',phone:'020-7878443443',email:'daisy.maersk@schwar.com'
-    },{
-        firstName:'David', lastName:'LiuXiao',phone:'0810-34766767',email:'daisvy@schwar.com'
-    },{
-        firstName:'Linux', lastName:'Apple',phone:'030-170965',email:'linxuapple@schwar.com'
-    }];
-
+export class ContactListComponent implements OnInit {
+      
     public selectedContact={};
+    public contacts:Contact[];
 
+    constructor(private _contactService:ContactService){
+
+    }
+      
     onSelect(contact){
         this.selectedContact=contact;
+    }
+
+    getContacts(){
+        this._contactService.getContacts().then((contacts:Contact[])=>{
+              this.contacts=contacts;
+        });
+    }
+
+    ngOnInit() {
+         this.getContacts();
     }
 }
---
+++ dev/contacts/mock-contact.ts
@@ -0,0 +1,13 @@
+import {Contact} from "./contact";
+
+export const CONTACTS:Contact[]=[{
+        firstName:'Max', lastName:'Schwarzuller',phone:'010-45443443',email:'max@schwar.com'
+    },{
+        firstName:'Jacob', lastName:'Chenxu',phone:'028-45443443',email:'jacob@schwar.com'
+    },{
+        firstName:'Dasiy', lastName:'Maersk',phone:'020-7878443443',email:'daisy.maersk@schwar.com'
+    },{
+        firstName:'David', lastName:'LiuXiao',phone:'0810-34766767',email:'daisvy@schwar.com'
+    },{
+        firstName:'Linux', lastName:'Apple',phone:'030-170965',email:'linxuapple@schwar.com'
+    }];
