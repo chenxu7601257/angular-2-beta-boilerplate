af2e1e93e369febe34386e5ae5fe10ed35539cbe
摄氏0℃ <chenxu7601257@qq.com>
2017-04-11 23:30:40 +0800

    angular2新特性 input #名字 路由
    angular2新特性 input #名字 路由private _router:Router
    跳转 this._router.navigate(['Contacts']);

--- dev/contacts/new-contact.component.ts
+++ dev/contacts/new-contact.component.ts
@@ -1,15 +1,61 @@
 import {Component, OnInit} from 'angular2/core';
 import {ContactComponent} from './contact.component';
 import {ContactService} from './contact.service';
-import {Contact} from './contact';
+import { Contact } from './contact';
+import { Router, ROUTER_DIRECTIVES } from "angular2/router";
 
 @Component({
     selector:'new-contact',
     template:`
-         New Contact Component
-    ` 
+       <div>
+            <div>
+                <label for='first-name'>First Name:</label>
+                <input type='text' id='first-name' #firstname><br/> 
+            </div>
+            <div>
+                <label for='last-name'>Last Name:</label>
+                <input type='text' id='last-name' #lastname><br/> 
+            </div>
+            <div>
+                <label for='phone'>Phone number:</label>
+                <input type='text' id='phone' #phone><br/> 
+            </div>
+            <div>
+                <label for='email'>E-Mail:</label>
+                <input type='text' id='email' #email><br/> 
+            </div> 
+        </div>
+        <br/> 
+        <button (click)="onAddContact(firstname.value,lastname.value,phone.value,email.value)">Create Contact</button>
+    `,
+    styles:[
+        `
+            label{
+                display:inline-block;
+                width:140px;
+            }
+            input{
+                width:250px;
+            }
+        `
+    ],
+    providers:[ContactService,ROUTER_DIRECTIVES]
 })
 
 export class NewContactComponent {
-     
+         
+    constructor(private _contactService:ContactService,private _router:Router){
+
+    }
+
+     onAddContact(firstName:string,lastName:string,phone:string,email:string){
+        const dummyContact={
+            firstName:firstName,
+            lastName: lastName,
+            phone:phone,
+            email:email
+         };
+         this._contactService.insertContact(dummyContact);
+         this._router.navigate(['Contacts']);
+     }
 }
